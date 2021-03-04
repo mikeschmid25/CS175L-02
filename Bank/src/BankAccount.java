fc0a794b5f54f189817551c5f48e104bc695aeb1
schmi <schmi@DESKTOP-EUQRGN9.fios-router.home>
2021-03-03 19:23:42 -0500

    Resubmission of first Bank Account project

--- Bank/src/BankAccount.java
+++ Bank/src/BankAccount.java
@@ -6,17 +6,17 @@
 	*/
 	public class BankAccount
 	{
-		 public double interestPct;
+		// public double interestPct;
 		
 	   private double balance;
 	   
 	   /**
 	      Constructs a bank account with zero balance.
 	   */
-	   public BankAccount(double amount, double interestAmount)
+	   public BankAccount(double amount) //double interestAmount)
 	   {
 	      balance = amount;
-	      interestPct = interestAmount;
+	      //interestPct = interestAmount;
 	   }
 
 	   /**
@@ -37,36 +37,36 @@
 	   {
 	      balance = balance - amount;
 	    
-	     if (balance < 0) {
-	    	  System.out.print("Insufficient Funds to support withdrawal");
+	    // if (balance < 0) {
+	    	  //System.out.print("Insufficient Funds to support withdrawal");
 	      } 
 
-	     else {
-	    	 System.out.print(balance);
-	     }
-	   }
+	     //else {
+	    	 //System.out.print(balance);
+	     //}
+	   //}
 	   /**
 	      Gets the current balance of this bank account.
 	      @return the current balance
 	   */
 	   
-	   public void calcInterest(double calculatedInterest) {
-		   double InterestAmount = interestPct * balance;
+	   //public void calcInterest(double calculatedInterest) {
+		  // double InterestAmount = interestPct * balance;
 		  
 		   
-	   }
-	   public double getInterest() {
-		   return interestPct * balance;
-	   }
+	   //}
+	   //public double getInterest() {
+		  // return interestPct * balance;
+	   //}
 	   
 	   public double getBalance()
 	   {
 	      return balance;
 	   }
-	   public double getBalancePlusInterest() {
-		   return balance + (interestPct * balance);
+	   //public double getBalancePlusInterest() {
+		  // return balance + (interestPct * balance);
 	   }
 	   
-	}
+	//}
 
 
--- Bank/src/BankAccountTester.java
+++ Bank/src/BankAccountTester.java
@@ -5,10 +5,10 @@ public class BankAccountTester {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter starting balance for this account: ");
 		double startBal = in.nextDouble();
-		System.out.println("Enter the interest rate for this account: ");
-		double interestRate = in.nextDouble();
+		//System.out.println("Enter the interest rate for this account: ");
+		//double interestRate = in.nextDouble();
 				//double Balance = startBal;
-		BankAccount myBankAccount = new BankAccount(startBal, interestRate);
+		BankAccount myBankAccount = new BankAccount(startBal); //interestRate);
 		
 			System.out.println("How much would you like to deposit?: ");
 			double depositAmount = in.nextDouble();
@@ -18,15 +18,15 @@ public class BankAccountTester {
 			double withDrawAmount = in.nextDouble();
 			myBankAccount.withdraw(withDrawAmount);
 			
-			System.out.println("If you want me to calculate interest, enter 'Yes': ");
-			String answer = in.next();
-				if (answer.equals("Yes")) {
-					System.out.print(myBankAccount.getInterest());
-				}
+			//System.out.println("If you want me to calculate interest, enter 'Yes': ");
+			//String answer = in.next();
+				//if (answer.equals("Yes")) {
+					//System.out.print(myBankAccount.getInterest());
+				//}
 			
 			//System.out.print(calcInterest);
 			
-			System.out.print("The account balance is: " + myBankAccount.getBalancePlusInterest());
+			System.out.print("The account balance is: " + myBankAccount.getBalance());//+ myBankAccount.getBalancePlusInterest());
 			
 	}}
 			
