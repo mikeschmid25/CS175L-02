3da0da3dd97dd08436c2d3bae55b136d35711b70
schmi <schmi@DESKTOP-EUQRGN9.fios-router.home>
2021-03-03 19:11:23 -0500

    The bank account code with interest modification

--- Bank/src/BankAccountTester.java
+++ Bank/src/BankAccountTester.java
@@ -5,19 +5,28 @@ public class BankAccountTester {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter starting balance for this account: ");
 		double startBal = in.nextDouble();
-		//double Balance = startBal;
-		BankAccount myBankAccount = new BankAccount(startBal);
+		System.out.println("Enter the interest rate for this account: ");
+		double interestRate = in.nextDouble();
+				//double Balance = startBal;
+		BankAccount myBankAccount = new BankAccount(startBal, interestRate);
 		
-		
+			System.out.println("How much would you like to deposit?: ");
+			double depositAmount = in.nextDouble();
+			myBankAccount.deposit(depositAmount);
+			
 			System.out.println("How much would you like to withdraw?: ");
 			double withDrawAmount = in.nextDouble();
 			myBankAccount.withdraw(withDrawAmount);
 			
-			System.out.println("How much owuld you like to deposit?: ");
-			double depositAmount = in.nextDouble();
-			myBankAccount.deposit(depositAmount);
+			System.out.println("If you want me to calculate interest, enter 'Yes': ");
+			String answer = in.next();
+				if (answer.equals("Yes")) {
+					System.out.print(myBankAccount.getInterest());
+				}
 			
-			System.out.println("Your current balance is: " + myBankAccount.getBalance());
+			//System.out.print(calcInterest);
+			
+			System.out.print("The account balance is: " + myBankAccount.getBalancePlusInterest());
 			
 	}}
 			
